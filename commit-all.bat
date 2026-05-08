e0e6e4c43d2d47aea96385a7f5edcc93c7050dd3
Michael Calkins <michaeljamescalkins@gmail.com>
2026-05-08 15:09:09 -0700

    chore: add all pending changes

--- commit-all.bat
+++ commit-all.bat
@@ -1,5 +1,5 @@
 @echo off
 del /f ".git\index.lock" 2>nul
 del /f ".git\HEAD.lock" 2>nul
-"C:\Program Files\Git\bin\bash.exe" -c "cd /c/Users/micha/Workspace/iCodeForBananas && git add -A && git commit -m 'chore: commit all pending changes including space math migration' && git push"
+"C:\Program Files\Git\bin\bash.exe" -c "cd /c/Users/micha/Workspace/iCodeForBananas && git add -A && git commit -m 'chore: add all pending changes' && git push"
 pause
