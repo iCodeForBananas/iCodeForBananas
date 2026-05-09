46ae1989d80053484000b5f9b6f3e1e3fed6e9d5
Michael Calkins <michaeljamescalkins@gmail.com>
2026-05-09 09:15:01 -0700

    chore: update commit-all.bat

--- commit-all.bat
+++ commit-all.bat
@@ -2,7 +2,7 @@
 cd /d "C:\Users\micha\Workspace\iCodeForBananas"
 del /f ".git\index.lock" 2>nul
 del /f ".git\HEAD.lock" 2>nul
-"C:\Program Files\Git\cmd\git.exe" add -A
-"C:\Program Files\Git\cmd\git.exe" commit -m "feat: strategy indicator overlays + remove TP from risk management UI"
+"C:\Program Files\Git\cmd\git.exe" add commit-all.bat
+"C:\Program Files\Git\cmd\git.exe" commit -m "chore: update commit-all.bat"
 "C:\Program Files\Git\cmd\git.exe" push
 pause
