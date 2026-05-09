dd74b2b5a46feda097a9ea3eead9e371822e9399
Michael Calkins <michaeljamescalkins@gmail.com>
2026-05-09 11:08:18 -0700

    chore: remove commit-all.bat

--- commit-all.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "C:\Users\micha\Workspace\iCodeForBananas"
-del /f ".git\index.lock" 2>nul
-del /f ".git\HEAD.lock" 2>nul
-"C:\Program Files\Git\cmd\git.exe" add .env.sample
-"C:\Program Files\Git\cmd\git.exe" add commit-all.bat
-"C:\Program Files\Git\cmd\git.exe" commit -m "chore: add .env.sample with all required env vars"
-"C:\Program Files\Git\cmd\git.exe" push
-pause
